a70eca17f819680a21cecd6ccf0a11c0899408f2
OmairaBarrera <lomaira.barrera@gmail.com>
2023-06-13 09:30:24 -0500

    feat: :card_file_box: Creation db
    This content bill, clients, products.

--- scripts/db/db_factura.sql
+++ scripts/db/db_factura.sql
@@ -1,8 +1,30 @@
-create DATABASE LuzBarrera;
-USE LuzBarrera;
-CREATE TABLE tb_personas(
-    cc INT(11) NOT NULL COMMENT 'Este campo donde se almacena la cedula',
-    nom_com VARCHAR(20) NOT NULL COMMENT 'Campo para almacenar el nombre completo',
-    edad INTEGER(3) NOT NULL COMMENT 'Campo para almacenar la edad',
-    PRIMARY KEY (cc)
+create DATABASE db_hunter_facture_LuzBarrera;
+USE db_hunter_facture_LuzBarrera;
+CREATE TABLE tb_bills(
+    n_bill INT(11) NOT NULL UNIQUE COMMENT 'Este el el campo donde se almacena el numero de la factura de o',
+    bill_data DATETIME NOT NULL DEFAULT NOW() UNIQUE COMMENT 'Campo apara almacenar la fecha actual de la factura',
+
+    PRIMARY KEY (n_bill) 
 );
+
+CREATE TABLE tb_clients(
+    cc BIGINT(20) NOT NULL PRIMARY KEY COMMENT 'Identificador del cliente',
+    fullname VARCHAR(255) NOT NULL COMMENT 'nombre del cliente',
+    email VARCHAR(255) NOT NULL COMMENT 'email del cliente',
+    address VARCHAR(255) NOT NULL COMMENT 'address del cliente',
+    phone VARCHAR(255) NOT NULL COMMENT 'numero tel del cliente'
+);
+
+CREATE TABLE tb_products(
+    id_product INT(11) NOT NULL UNIQUE AUTO_INCREMENT COMMENT 'Identificador del product',
+    name VARCHAR(50) NOT NULL COMMENT 'nombre del product',
+    amount INT NOT NULL COMMENT 'amount del product',
+    price NUMERIC(6,2) NOT NULL COMMENT 'Value del product',
+
+    PRIMARY KEY (id_product)
+);
+
+DROP TABLE tb_products;
+ALTER TABLE tb_personas MODIFY COLUMN nom_com VARCHAR(60);
+
+TRUNCATE TABLE tb_personas;
