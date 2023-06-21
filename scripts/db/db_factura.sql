56151e558f8987fd373e7d7c0097cad2a1be6b66
OmairaBarrera <lomaira.barrera@gmail.com>
2023-06-21 06:19:14 -0500

    feat: :sparkles: new schemes

--- scripts/db/db_factura.sql
+++ scripts/db/db_factura.sql
@@ -1,8 +1,12 @@
+
+--Creacion de la base de catos
 create DATABASE db_hunter_facture_LuzBarrera;
+--Usar la base de datos creada
 USE db_hunter_facture_LuzBarrera;
+--Creacion de una tabla en la base de datos que estamos utilizando
 CREATE TABLE tb_bills(
-    n_bill INT(11) NOT NULL UNIQUE COMMENT 'Este el el campo donde se almacena el numero de la factura de o',
-    bill_data DATETIME NOT NULL DEFAULT NOW() UNIQUE COMMENT 'Campo apara almacenar la fecha actual de la factura',
+    n_bill INT(11) NOT NULL UNIQUE COMMENT 'Este es el campo donde se almacena el numero de la factura',
+    bill_data DATETIME NOT NULL DEFAULT NOW() UNIQUE COMMENT 'Campo para almacenar la fecha actual de la factura',
 
     PRIMARY KEY (n_bill) 
 );
@@ -25,6 +29,17 @@ CREATE TABLE tb_products(
 );
 
 DROP TABLE tb_products;
+--Modificar el tipo de dato  de una columna en la tabla de tb_personas
 ALTER TABLE tb_personas MODIFY COLUMN nom_com VARCHAR(60);
 
 TRUNCATE TABLE tb_personas;
+
+--Damos uso a otra base de datos
+USE db_hunter_facture;
+--Insertamos datos en la tabla tb_client
+INSERT INTO tb_client(identificacion, full_name, email, address, phone) VALUES("1098820098", "Omaira Barrera", "Omaira@gmail.com", "Carrera 32", "3223024631");
+--Consulta para obtener todos los registros de la tabla tb_client
+SELECT * FROM tb_client;
+--Ordenar una tabla segun una condicion
+SELECT * FROM tb_client ORDER BY full_name;
+
